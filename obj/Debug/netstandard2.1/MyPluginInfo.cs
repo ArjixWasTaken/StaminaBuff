1a4f03c94fa56f4dd72c78732177ae3bbcff1728
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2023-12-10 18:25:24 +0200

    migrate to template

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace StaminaBuff
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "StaminaBuff";
+        public const string PLUGIN_NAME = "StaminaBuff";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
